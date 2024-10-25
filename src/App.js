a98c7d44632b5730624870634407278acb04e96c
maryanfawzy <maryanfawzy7@gmail.com>
2024-10-25 17:42:26 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import AllRoutes from './routes/AllRoutes';
 import './App.css';
 import Header  from './components/Header'
 import  MovieList from './pages/MovieList';
-import {Footer} from './components/Footer'
+import Footer from './components/Footer'
 
 
 
---
+++ src/components/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    
+
+
+
+<footer className ="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <div className ="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span className ="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className ="hover:underline">Cenima</Link>. All Rights Reserved.
+    </span>
+    <ul className ="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="#" target='_blank' className ="hover:underline me-4 md:me-6">Instasgram</a>
+        </li>
+        <li>
+            <a href="#" target='_blank' className ="hover:underline me-4 md:me-6">LinkedIn</a>
+        </li>
+        <li>
+            <a href="#" target='_blank' className ="hover:underline me-4 md:me-6">GitHub</a>
+        </li>
+        <li>
+            <a href="#" target='_blank' className ="hover:underline">Contact us</a>
+        </li>
+    </ul>
+    </div>
+</footer>
+
+
+
+  )
+}
+
+export default Footer
